9e9dfe20c8c6e1d54d5675f8aa137ba796963abe
Wilfred Dube <wilfreddube@gmail.com>
2017-07-22 18:28:31 +0200

    Remove port=8889; for poratability

--- classes/database.php
+++ classes/database.php
@@ -15,7 +15,7 @@ class Database extends PDO {
 
 		try {
 			// create PDO connection
-			$this->db = new PDO ( "mysql:host=".DBHOST.";port=8889;dbname=".DBNAME.";charset=utf8", DBUSER, DBPASS );
+			$this->db = new PDO ( "mysql:host=".DBHOST.";dbname=".DBNAME.";charset=utf8", DBUSER, DBPASS );
 			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 		} catch ( PDOException $e ) {
